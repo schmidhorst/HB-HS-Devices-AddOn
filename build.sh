646465b45e1da2140be9ded172908fd57a5219c5
Horst <info@schmidhorst.de>
2023-10-22 21:32:39 +0200

    First Clean-Up
    Deleted not required files

--- build.sh
+++ build.sh
@@ -1,43 +1,44 @@
 #!/bin/sh
-COPYFILE_DISABLE=1; export COPYFILE_DISABLE
-ADDON_NAME=jp-hb-devices
+#COPYFILE_DISABLE=1; export COPYFILE_DISABLE
+ADDON_NAME=hb-hs-devices-addon
 
-file=./patchsource/www/rega/esp/functions.fn
-diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
-file=./patchsource/www/rega/esp/controls/rhs.fn
-diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
-file=./patchsource/www/pda/eq3/controls/rhs.tcl
-diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
-file=./patchsource/www/webui/webui.js
-diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
-file=./patchsource/www/config/ic_common.tcl
-diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
-file=./patchsource/www/rega/esp/datapointconfigurator.fn
-diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
-file=./patchsource/www/rega/esp/channels.fn
-diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
-file=./patchsource/www/webui/js/extern/sliderControl.js
-diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
+# file=./patchsource/www/rega/esp/functions.fn
+# diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
+# file=./patchsource/www/rega/esp/controls/rhs.fn
+# diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
+# file=./patchsource/www/pda/eq3/controls/rhs.tcl
+# diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
+# file=./patchsource/www/webui/webui.js
+# diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
+# file=./patchsource/www/config/ic_common.tcl
+# diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
+# file=./patchsource/www/rega/esp/datapointconfigurator.fn
+# diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
+# file=./patchsource/www/rega/esp/channels.fn
+# diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
+# file=./patchsource/www/webui/js/extern/sliderControl.js
+# diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
 
-file=./patchsource/www/rega/pages/tabs/admin/views/programs.htm
-diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/le_343/`basename ${file}`.patch 
-diff -u --label=${file}.orig --label=${file} ${file}.orig-3.45 ${file}-3.45 > ./src/addon/patch/ge_345/`basename ${file}`.patch
-file=./patchsource/www/rega/esp/side.inc
-diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/le_343/`basename ${file}`.patch 
-diff -u --label=${file}.orig --label=${file} ${file}.orig-3.45 ${file}-3.45 > ./src/addon/patch/ge_345/`basename ${file}`.patch
+# file=./patchsource/www/rega/pages/tabs/admin/views/programs.htm
+# diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/le_343/`basename ${file}`.patch 
+# diff -u --label=${file}.orig --label=${file} ${file}.orig-3.45 ${file}-3.45 > ./src/addon/patch/ge_345/`basename ${file}`.patch
+# file=./patchsource/www/rega/esp/side.inc
+# diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/le_343/`basename ${file}`.patch 
+# diff -u --label=${file}.orig --label=${file} ${file}.orig-3.45 ${file}-3.45 > ./src/addon/patch/ge_345/`basename ${file}`.patch
 
-echo "Patch created."
+# echo "Patch created."
 
 rm ${ADDON_NAME}-addon.tgz
 cd src
-chmod +x update_script
-chmod +x addon/install_*
-chmod +x addon/uninstall_*
-chmod +x addon/update-check.cgi
-chmod +x rc.d/*
-find . -name ".DS_Store" -exec rm -rf {} \;
-find . -name "._*" -exec rm -rf {} \;
-dot_clean .
+chmod 755 update_script
+chmod 755 addon/install_*
+chmod 755 addon/uninstall_*
+chmod 755 addon/update-check.cgi
+chmod 755 addon/patchworker
+chmod 755 rc.d/*
+#find . -name ".DS_Store" -exec rm -rf {} \;
+#find . -name "._*" -exec rm -rf {} \;
+#dot_clean .
 tar --exclude='untitled.txt' -zcf ../${ADDON_NAME}-addon.tgz * 
 echo "tar created."
 cd ..
