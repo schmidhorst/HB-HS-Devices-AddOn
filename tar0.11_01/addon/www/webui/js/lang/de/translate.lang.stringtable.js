0b1b7324a0a10c961bea3c16fd241d060c7196b8
schmidhorst <info@schmidhorst.de>
2023-10-29 18:42:45 +0100

    Delete tar0.11_01 directory

--- tar0.11_01/addon/www/webui/js/lang/de/translate.lang.stringtable.js
+++
@@ -1,8 +0,0 @@
-jQuery.extend(true,langJSON, {
-  "de" : {
-    "dummy" : "",
-    "stringTableTemperatureOffset" : "Temperatur-Offset",
-    "stringTableWeatherAddress" : "1WireAdresse (optional, 4 Byte / 8 Zeichen)",
-    "noMoreKeys" : ""
-  }
-});
